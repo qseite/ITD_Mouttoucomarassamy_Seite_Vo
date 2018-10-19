3baf32d31591c167c6bec04664e2ede49c58a8f9
romai <romai@172.17.2.4>
2018-10-19 14:29:04 +0200

    Romain - mutation

--- tsp-framework-master/src/tsp/Individu.java
+++ tsp-framework-master/src/tsp/Individu.java
@@ -17,6 +17,10 @@ public class Individu {
 	public ArrayList<Integer> getOrdreVisite(){
 		return this.ordreVisite;
 	}
+	
+	public Instance getInstance() {
+		return this.g_instance;
+	}
 
 	public double getValeur() throws Exception {
 		double res=0;
@@ -36,7 +40,34 @@ public class Individu {
 			ordre.add(ind1.getOrdreVisite().get(index));
 		}
 		
+		
 		Individu res = new Individu(this.g_instance,ordre);
 		return res;
+	}
+	
+	public void mutation() {
+		int nombreVille=this.getInstance().getNbCities();
+		int index1=(int)(Math.random()*nombreVille)-1;
+		int index2=index1+(int)(Math.random()*(nombreVille-index1-1));
+		int index3=index2+(int)(Math.random()*(nombreVille-index2-1));
+		int index4=index2+(int)(Math.random()*(nombreVille-index3-1));
+		ArrayList<Integer> mutation = new ArrayList<Integer>();
+		for (int i=0;i<index1;i++) {
+			mutation.add(this.getOrdreVisite().get(i));
+		}
+		for (int i=index1;i<index2;i++) {
+			mutation.add(this.getOrdreVisite().get(i));
+		}
+		for (int i=index2;i<index3;i++) {
+			mutation.add(this.getOrdreVisite().get(i));
+		}
+		for (int i=index3;i<index4;i++) {
+			mutation.add(this.getOrdreVisite().get(i));
+		}
+		for (int i=index4;i<nombreVille;i++) {
+		    mutation.add(this.getOrdreVisite().get(i));
+		}
+		
+		this.ordreVisite=mutation;
 	}
 }
