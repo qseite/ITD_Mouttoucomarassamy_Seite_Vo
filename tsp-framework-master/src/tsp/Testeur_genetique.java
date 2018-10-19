a2e0eaa0169561db6e9f6fddf33c32ed4441ddc2
Utilisateur <Utilisateur@172.17.3.176>
2018-10-19 16:12:48 +0200

    modif testeur genetique

---
+++ tsp-framework-master/src/tsp/Testeur_genetique.java
@@ -0,0 +1,16 @@
+package tsp;
+
+public class Testeur_genetique() {
+	
+	public static void main(String[] args) {
+		
+		int nbIndividus = 50;
+		Instance g_instance = new Instance("instances/eil51.tsp",0);
+		Population population = new Population(nbIndividus,g_instance);
+		
+		
+	}
+}
+}
+
+
