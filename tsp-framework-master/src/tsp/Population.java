0d508da118d3a22bdfd1765047371bb6a4bb6e6a
Utilisateur <Utilisateur@172.17.3.176>
2018-10-19 14:39:09 +0200

    fin insertion et getBest dans population

--- tsp-framework-master/src/tsp/Population.java
+++ tsp-framework-master/src/tsp/Population.java
@@ -11,19 +11,30 @@ public class Population {
 	}
 	
 	//retourne le meilleur individu
-	public Individu getBest() {
-		double valeur=this.population.get(0);
+	public Individu getBest() throws Exception {
+		double valeur=this.population.get(0).getValeur();
+		int index=0;
 		for (int i=1;i<this.population.size();i++) {
 			double inter = this.population.get(i).getValeur();
 			if (inter<valeur){
-				res=valeur;
+				index=i;
 			}
-		}
+		}return this.population.get(index);
 	}
 	
+	
 	// insere l'individu en argument et enlève et retourne l'individu retiré (celui qui a la plus grande distance)
-	public Individu insertion(Individu inserer) {
-		
-	}
-
+	public Individu insertion(Individu aInserer) throws Exception {
+		double valeur=this.population.get(0).getValeur();
+		int index=0;
+		for (int i=1;i<this.population.size();i++) {
+			double inter = this.population.get(i).getValeur();
+			if (inter>valeur){
+				index=i;
+			}
+	    }Individu res = this.population.get(index);
+		this.population.remove(index);
+		this.population.add(aInserer);
+		return res;
+     }
 }
