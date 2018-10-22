6829b652ab6ead63610feca4f6416f42f17f7745
romai <romai@172.16.121.123>
2018-10-22 10:06:53 +0200

    Transfert crossover --> population Refactor TesteurGenetique

--- tsp-framework-master/src/tsp/Individu.java
+++ tsp-framework-master/src/tsp/Individu.java
@@ -7,7 +7,6 @@ public class Individu {
 	private Instance g_instance;
 	private ArrayList<Integer> ordreVisite;
 	
-
 	
 	public Individu(Instance g_instance, ArrayList<Integer> ordreVisite) {
 		this.g_instance=g_instance;
@@ -33,37 +32,7 @@ public class Individu {
 		return res;
 	}
 	
-	public Individu crossover(Individu ind1, Individu ind2) {
-		ArrayList<Integer> ordre = new ArrayList<Integer>();
-		int taille = ind1.getOrdreVisite().size();
-		int alea = ((int)Math.random()*taille)-1;
-		int index=0;
-		while (index<alea) {
-			ordre.add(ind1.getOrdreVisite().get(index));
-		}
-		
-		int i=index+1;
-		int tailleIntermediaire=ordre.size();
-		while(index<taille) {
-			int compt=0;
-			boolean present = false;
-			while (compt<tailleIntermediaire) {
-				if(ind2.getOrdreVisite().get(i)==ordre.get(compt)) {
-					present=true;
-				}
-			}if (present==false) {
-				ordre.add(ind2.getOrdreVisite().get(i));
-			}
-			if (i<taille) {
-				i=i+1;
-			}else {
-				i=0;
-			}index++;
-		}
 
-		Individu res = new Individu(this.g_instance,ordre);
-		return res;
-	}
 	
 	public void mutation() {
 		int nombreVille=this.getInstance().getNbCities();
---
+++ tsp-framework-master/src/tsp/TesteurGenetique.java
@@ -0,0 +1,18 @@
+package tsp;
+
+import java.io.IOException;
+
+public class TesteurGenetique {
+	
+	public static void main(String[] args) throws IOException {
+		
+		int nbIndividus = 50;
+		Instance g_instance = new Instance("instance/eil51.tsp",0);
+		Population population = new Population(nbIndividus,g_instance);
+		int rien =0;
+		
+	}
+}
+
+
+
