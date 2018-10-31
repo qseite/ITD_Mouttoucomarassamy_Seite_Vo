32a3e8ee7b2e01d6b82a030d6a455ceb4677ed38
Utilisateur <Utilisateur@LAPTOP-R762718E.home>
2018-10-31 13:24:09 +0100

    remise en ordre

--- tsp-framework-master/src/tsp/LocalSearch.java
+++ tsp-framework-master/src/tsp/LocalSearch.java
@@ -1,38 +1,39 @@
 package tsp;
- 
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
 
 public class LocalSearch {
 	private Instance instance;
 	private int[] solution;
 	private int[] temp;
+	private int[] ini; // permet uniquement de comparer si la solution optimale a évoluer au cours d'une itération
+	// on stop l'algo lorsque la solution ne s'améliore pas au bout d'une itération
+	private double gain;
 	
 
-	public LocalSearch(Instance instance) {
-
+	public LocalSearch(Instance instance) throws Exception {
 		super();
 		this.instance = instance;
 		this.temp= new int[this.instance.getNbCities()];
-
+		this.solution= new int[this.instance.getNbCities()];
+		this.ini=new int[this.instance.getNbCities()];
 		ArrayList<Integer> sol= new ArrayList<Integer>();	
 		for(int i=0;i<instance.getNbCities();i++) {
 			sol.add(i);
 		}
 		Collections.shuffle(sol);
-		int[] solution= new int[instance.getNbCities()];
 		for(int j=0;j<instance.getNbCities();j++) {
-			solution[j]=sol.get(j);
+			this.ini[j]=sol.get(j);
+			this.solution[j]=sol.get(j);
 		}
-		this.solution=solution;
-
+		this.gain=0;
+		
 	}
 	public void swap(int[] tab, int i,int j) {
 		int a=tab[i];
-		tab[i]=tab[j];
+		int b=tab[j];
+		tab[i]=b;
 		tab[j]=a;
 	}
 	
@@ -44,18 +45,57 @@ public class LocalSearch {
 		return this.solution;
 	}
 	
+	public void setSolution(int[] tab) {
+		for (int i=0;i<tab.length;i++) {
+			this.solution[i]=tab[i];
+		}
+	}
+	
+	public void setSolution(int index, int val) {
+		this.solution[index]=val;
+	}
+	
+	public void swapSolution(int i1,int i2) {
+		this.swap(this.solution, i1, i2);
+	}
+	
 	public int[] getTemp() {
 		return this.temp;
 	}
 	
+	public void setTemp(int[] tab) {
+		for (int i=0;i<tab.length;i++) {
+			this.temp[i]=tab[i];
+		}
+	}
+	
+	public int[] getIni() {
+		return this.ini;
+	}
+	
+	public void setIni(int[] tab) {
+		for (int i=0;i<tab.length;i++) {
+			this.ini[i]=tab[i];
+		}
+	}
+	
+	public double getGain() {
+		return this.gain;
+	}
+	
+	public void setGain(double g) {
+		this.gain=g;
+	}
+
 	public double distance(int[] tab) throws Exception {
 		double d=0;
 		for (int i=0;i<tab.length-1;i++) {
 			d=d+this.instance.getDistances(tab[i],tab[i+1]);
 		}
-		d=d+this.instance.getDistances(tab[tab.length-1],tab[0]);		
+		d+=this.instance.getDistances(tab[0], tab[tab.length-1]);
 		return d;
 	}
+	
 	
 	public String tostring(int[] tab) {
 		String stringsol="";
@@ -65,59 +105,83 @@ public class LocalSearch {
 		return stringsol;
 	}
 
-	
 	public void swapCycle(int index) throws Exception { //réalise un cycle de swap sur l'élement d'index 'index'
-		for (int i=0;i<index;i++) {
+		int n = this.getInstance().getNbCities();
+
+		for (int i=index+1;i<this.getInstance().getNbCities();i++) {
+			this.setTemp(this.getIni());
+			
+			double g1=this.getInstance().getDistances(this.getIni()[(n+index-1)%n],this.getIni()[index])
+					+this.getInstance().getDistances(this.getIni()[index], this.getIni()[(index+1)%n]);
+			double d1=this.getInstance().getDistances(this.getIni()[(n+i-1)%n],this.getIni()[i])
+					+this.getInstance().getDistances(this.getIni()[i], this.getIni()[(i+1)%n]);
+			//distance au voisinage des points encore non swapés
+			
 			this.swap(this.temp, i, index);
-			if (this.distance(this.getTemp())<this.distance(this.getSolution())) {
-				this.solution=this.temp;
+
+			double g2=this.getInstance().getDistances(this.getTemp()[(n+index-1)%n],this.getTemp()[index])
+					+this.getInstance().getDistances(this.getTemp()[index], this.getTemp()[(index+1)%n]);
+			double d2=this.getInstance().getDistances(this.getTemp()[(n+i-1)%n],this.getTemp()[i])
+					+this.getInstance().getDistances(this.getTemp()[i], this.getTemp()[(i+1)%n]);
+			//distance au voisinage des points swapés		
+			
+			//if (this.distance(this.getTemp())<this.distance(this.getSolution())) {
+			if (g1+d1-g2-d2>this.getGain()) { 
+			//plus la somme est importante plus g2 et d2 son petits donc plus la distance de la nvlle sol est courte
+				this.setSolution(this.getTemp());
+				this.setGain(g1+d1-g2-d2);
 			}
+			
+
 		}
 	}
 	
-	public static void main(String[] args)  {
-			try{
-				Instance graph = new Instance("instances/eil10.tsp",0);
-				LocalSearch LS=new LocalSearch(graph);
-				int[] tab = LS.solution; //0765349812
-				int j;
-				int[] boucletab;
-				int indexj; /* index de j repositionné pour parcourir la liste de i+1 à i-1*/
-				double distanceMin=LS.distance(tab);
-				boolean condition=true;/*condition d'arret de la recherche locale*/
-				int i=0;/*indice qui va être swappé en position indexj*/
-				double verifcondition=distanceMin;
-				while(condition) {
-					while(i<tab.length){ //on prend tous les i du tableau pour les swaps a des emplacements indexj
-						j=i+1;
-						indexj=j%tab.length;
-						while(indexj!=i) {//tant qu'on revient pas à l'emplacement de départ, on swap
-							boucletab=tab;//copie du tableau d'origine
-							LS.swap(boucletab,i,indexj);
-							//System.out.println("distance actuelle "+LS.distance(boucletab));
-							//System.out.println("distanceMin trouvée "+distanceMin);
-							if(LS.distance(boucletab)<distanceMin) {//on compare les distances pour voir si on trouve mieux
-								LS.solution=boucletab;
-								System.out.println(LS.tostring(LS.solution));
-								distanceMin=LS.distance(LS.solution);
-								System.out.println("Nouvelle solution:"+LS.tostring(LS.solution)+" : "+LS.distance(LS.solution)+"km");
-							}
-							indexj=(indexj+1)%tab.length;
-						}
-						i=i+1;
-					}
-					if(verifcondition==distanceMin) {
-						condition=false;
-					}else {
-						verifcondition=distanceMin;
-					}
-				tab=LS.solution;//on mémorise le changement si il y en a eu
-			}
+	public void swapIteration() throws Exception {
+		for (int i=0; i<this.getInstance().getNbCities();i++) {
+			this.swapCycle(i);
+		}		
+	}
+
+	public void setInitial() {
+		this.setIni(this.getSolution());
+	}
+	
+	// on vérifie que pendant une itération la solution finale de l'itération à changer de la solution initiale
+	public boolean iniEqualsSol() throws Exception { 
+		return (this.distance(this.getIni())==this.distance(this.getSolution()));
+	}
+	
+	public static void main(String[] args) throws Exception {
+		boolean testEgalite=true; 
+		long t0;
+		long t1=0;
+		int k=0;
+		t0=System.currentTimeMillis();
+		Instance graph = new Instance("instances/d657.tsp",0);
+		LocalSearch ls = new LocalSearch(graph);
+		System.out.println("Solution initiale : "+ls.tostring(ls.getIni()));
+		System.out.println("Distance initiale : "+ls.distance(ls.getIni()));
+		
+		do {
+			ls.swapIteration();
+			ls.setGain(0);
+			System.err.println(ls.distance(ls.getSolution()));
 			
-				System.out.println(LS.tostring(LS.solution));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			if (ls.iniEqualsSol()) {
+				testEgalite=false;
+			}
+			//System.out.println(k);
+			k++;
+			ls.setInitial();
+			t1=System.currentTimeMillis();
+		} while (testEgalite && (t1-t0)<20000); /* condition d'arrêt de l'algo
+		un tour de boucle correspond à une itération de l'algo, O(n) */
+		
+		System.out.println("Solution finale : "+ls.tostring(ls.getSolution()));
+		System.out.println("Distance finale : "+ls.distance(ls.getSolution()));
+		System.out.println("Durée totale d'exécution : "+(t1-t0)+" ms");
+		System.out.println("Durée d'une itération : "+(t1-t0)/k+ "ms");
+		System.out.println("Nombre d'itération : "+k);
+		System.out.println("Arrêt de l'algo car ini==sol : "+!testEgalite);
 	}
 }
