253621fae21d3a724b48ea0172aa5e222de123de
romai <romai@192.168.1.37>
2018-11-01 15:44:19 +0100

    derniere maj gen sur testeur

--- tsp-framework-master/src/tsp/TesteurGenetique.java
+++ tsp-framework-master/src/tsp/TesteurGenetique.java
@@ -15,7 +15,7 @@ public class TesteurGenetique {
 		Instance g_instance = new Instance("instances/eil51.tsp",0);
 		Population population = new Population(nbIndividus,g_instance);
 		int nbIterationsElitistes=5;
-		int nbIterationsNonElitistes=500;
+		int nbIterationsNonElitistes=50000;
 		double seuilMutation = 0.2;
 		Individu meilleur = population.getBest();
 		double meilleur_valeur=meilleur.getValeur();
@@ -23,7 +23,7 @@ public class TesteurGenetique {
 		//Iterations avec une s�lection des parents �litiste et sans mutation
 		for(int i=0;i<nbIterationsElitistes;i++)  {
 			System.out.println("iteration:"+i);
-			ArrayList<Individu> parents = population.selectionElitiste();
+			ArrayList<Individu> parents = population.selectionRoulette();
 			Individu enfant = population.crossover2(parents.get(0),parents.get(1));
 			population.insertion(enfant);
 			if (meilleur.getValeur()>population.getBest().getValeur()) {
@@ -36,7 +36,7 @@ public class TesteurGenetique {
 		//It�rations avec une s�lection des parents al�atoire et avec mutation
 		for(int i=nbIterationsElitistes;i<nbIterationsNonElitistes+nbIterationsElitistes;i++) {
 			System.out.println("iteration:"+i);
-			ArrayList<Individu> parents = population.selectionAleatoire();
+			ArrayList<Individu> parents = population.selectionRoulette();
 			Individu enfant = population.crossover2(parents.get(0),parents.get(1));
 			double alea = Math.random();
 			if(alea<seuilMutation) {
@@ -45,8 +45,9 @@ public class TesteurGenetique {
 			population.insertion(enfant);
 			if (meilleur.getValeur()>population.getBest().getValeur()) {
 				meilleur=population.getBest();
+				meilleur_valeur=meilleur.getValeur();
 			}
-			System.out.println("valeur: -----"+population.getBest().getValeur()+"----");	
+			System.out.println("valeur: -----"+meilleur_valeur+"----");	
 			}
 		
 		t1=System.currentTimeMillis();
--- tsp-framework-master/src/tsp/TwoOpt.java
+++ tsp-framework-master/src/tsp/TwoOpt.java
@@ -13,23 +13,6 @@ public class TwoOpt extends LocalSearch{
 	}
 	
 	/*
-	public int[] cavacouper(int[] tab,int gauche,int droite) {
-		ArrayList<Integer> coupe= new ArrayList<Integer>();
-		ArrayList<Integer> tablist= new ArrayList<Integer>();
-		for(int i=0;i<tab.length;i++) {
-			tablist.add(tab[i]);
-		}
-		coupe=(ArrayList<Integer>) tablist.subList(gauche, droite+1);
-		tablist.removeAll(coupe);
-		Collections.reverse(coupe);
-		tablist.addAll(gauche,coupe);
-		for(int i=0;i<tab.length;i++) {
-			tab[i]=tablist.get(i);
-		} 
-		return tab;
-	}*/
-	
-	/*
 	 * @param int index : on réalise une série de 2-opt entre index et les villes [index+1;n[
 	 * Réalise une série de renversement 2-opt entre 'index' et 'nombreDeVilles'
 	 */
